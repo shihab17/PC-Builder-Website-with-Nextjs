42c0fe0468d4b7b10b08a1411575c6e6bcb175be
shihab17 <shihabahmed620@gmail.com>
2023-08-02 01:11:03 +0600

    get all product data

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,3 +14,14 @@ export default Home;
 Home.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://pc-builder-api-shihab17.vercel.app/api/v1/product');
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: {
+      allProducts: data?.data
+    }
+  }
+}
